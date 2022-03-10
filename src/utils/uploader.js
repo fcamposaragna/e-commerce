da54d791782921763f4fde81cb1739bc97e0eb50
fcamposaragna <fcamposaragna@gmail.com>
2022-03-10 19:58:03 -0300

    many changes

--- src/utils/uploader.js
+++ src/utils/uploader.js
@@ -1,11 +1,12 @@
 import multer from 'multer';
+import config from '../config/config.js'
 
 const storage = multer.diskStorage({
     destination(req,file,cb){
         cb(null,'src/public/images')
     },
     filename:(req,file,cb)=>{
-        cb(null,Date.now()+file.originalname);
+        cb(null,req.protocol + req.hostname + ":" + (process.env.PORT||8080) + "/images" + file.filename);
     }
 })
 const uploader = multer({storage:storage});
